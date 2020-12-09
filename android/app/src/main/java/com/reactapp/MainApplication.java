02313776e94374c790eb2f4c5ae8ece2fc8324d7
Manu <mannujindal9034@gmail.com>
2020-12-09 15:56:31 +0530

    Remove Key chain

--- android/app/src/main/java/com/reactapp/MainApplication.java
+++ android/app/src/main/java/com/reactapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.keychain.KeychainPackage;
 import com.kishanjvaghela.cardview.RNCardViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
